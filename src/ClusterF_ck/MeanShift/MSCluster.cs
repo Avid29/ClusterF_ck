68a2ad16047219a6b2be1104b13cad1c1b9876bf
Adam Dernis <avid29@live.com>
2022-10-28 12:20:05 -0400

    Added MeanShift

---
+++ src/ClusterF_ck/MeanShift/MSCluster.cs
@@ -0,0 +1,35 @@
+﻿// Adam Dernis © 2022
+
+using ClusterF_ck.IO;
+using ClusterF_ck.IO.Interfaces;
+using ClusterF_ck.Spaces.Properties;
+
+namespace ClusterF_ck.MeanShift
+{
+    /// <summary>
+    /// A <see cref="Cluster{T, TShape}"/> implementation for the MeanShift algorithm.
+    /// </summary>
+    /// <typeparam name="T">The type of points in the cluster.</typeparam>
+    /// <typeparam name="TShape">A shape to describe to provide comparison methods for <typeparamref name="T"/>.</typeparam>
+    public class MSCluster<T, TShape> : Cluster<T, TShape>, ICentroidCluster<T>, IWeightedCluster
+        where T : unmanaged
+        where TShape : struct, IDistanceSpace<T>, IWeightedAverageSpace<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MSCluster{T, TShape}"/> class.
+        /// </summary>
+        /// <param name="centroid">The centroid of the <see cref="MSCluster{T, TShape}"/>.</param>
+        /// <param name="weight">The total weight of, or summed point count for the cluster.</param>
+        internal MSCluster(T centroid, double weight)
+        {
+            Centroid = centroid;
+            Weight = weight;
+        }
+
+        /// <inheritdoc/>
+        public T Centroid { get; }
+
+        /// <inheritdoc/>
+        public double Weight { get; }
+    }
+}
---
+++ src/ClusterF_uck.Base/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿// Adam Dernis © 2022
+
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("ClusterF_ck")]

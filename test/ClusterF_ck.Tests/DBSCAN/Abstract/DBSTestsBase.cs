6c955623e7bf9422e433f9edb32925e9a37be77d
Adam Dernis <avid29@live.com>
2022-10-28 11:32:53 -0400

    Added DBSCAN run tests

---
+++ test/ClusterF_ck.Tests/DBSCAN/Abstract/DBSTestsBase.cs
@@ -0,0 +1,20 @@
+﻿// Adam Dernis © 2022
+
+using ClusterF_ck.DBSCAN;
+using ClusterF_ck.Spaces.Properties;
+using ClusterF_ck.Tests.Data.DataSet.Abstract;
+using DBS = ClusterF_ck.DBSCAN.DBSCAN;
+
+namespace ClusterF_ck.Tests.DBSCAN.Abstract
+{
+    public abstract class DBSTestsBase
+    {
+        protected void Run<T, TShape>(DataSet<T> data, DBSConfig<T, TShape> config, TShape shape = default)
+            where T : unmanaged
+            where TShape : struct, IDistanceSpace<T>
+        {
+            // TODO: Verify results
+            DBS.Cluster(data.Data, config, shape);
+        }
+    }
+}
---
+++ test/ClusterF_ck.Tests/DBSCAN/DBSFixedTests.cs
@@ -0,0 +1,33 @@
+﻿// Adam Dernis © 2022
+
+using ClusterF_ck.DBSCAN;
+using ClusterF_ck.Shapes;
+using ClusterF_ck.Tests.Data.DataSet;
+using ClusterF_ck.Tests.DBSCAN.Abstract;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Numerics;
+
+namespace ClusterF_ck.Tests.DBSCAN
+{
+    [TestClass]
+    public class DBSFixedTests : DBSTestsBase
+    {
+        [TestMethod("Double Test 1")]
+        public void DoubleTest1()
+        {
+            var data = FixedSets.Double_Test1;
+            var config = new DBSConfig<double, DoubleShape>(2, 1, true);
+
+            Run(data, config);
+        }
+
+        [TestMethod("Vector2 Test 1")]
+        public void Vector2Test1()
+        {
+            var data = FixedSets.Vector2_Test1;
+            var config = new DBSConfig<Vector2, Vector2Shape>(2, 1, true);
+
+            Run(data, config);
+        }
+    }
+}

40a5e1df0c5f664f5dad8303080511bd8cc1e1d1
Adam Dernis <Avid29@Live.com>
2023-10-06 16:30:32 -0400

    WIP Added higher level of generics for GPU ClusterRaw

--- src/ClusterF_ck.GPU/MeanShift/MeanShift.Private.cs
+++ src/ClusterF_ck.GPU/MeanShift/MeanShift.Private.cs
@@ -1,31 +1,49 @@
 ﻿// Adam Dernis © 2023
 
 using ClusterF_ck.GPU.MeanShift.Shaders;
+using CommunityToolkit.Diagnostics;
 using ComputeSharp;
 
 namespace ClusterF_ck.GPU.MeanShift;
 
 public partial class MeanShift
 {
-    internal static void ClusterRaw(
-        ReadWriteBuffer<float> pointsBuffer,
-        ReadOnlyBuffer<float> fieldBuffer,
+    internal static void ClusterRaw<T>(
+        ReadWriteBuffer<T> pointsBuffer,
+        ReadOnlyBuffer<T> fieldBuffer,
         Func<float, float> kernel)
+        where T : unmanaged
     {
         var device = pointsBuffer.GraphicsDevice;
-        var fieldWeights = device.AllocateReadWriteTexture2D<float>(pointsBuffer.Length, fieldBuffer.Length);
+        var fieldWeights = device.AllocateReadWriteTexture2D<T>(pointsBuffer.Length, fieldBuffer.Length);
         var flagBuffer = device.AllocateReadWriteBuffer<int>(1);
         var source = device.AllocateReadOnlyBuffer(new [] { 0 });
         var readBack = device.AllocateReadBackBuffer<int>(1);
 
-        var shader = new MeanShiftFloatPointsShader(pointsBuffer, fieldBuffer, fieldWeights, flagBuffer, kernel);
+        // Create a shader to match the method type
+        T type = default;
+        Action? step = type switch
+        {
+            float _ => () => device.For(pointsBuffer.Length,
+                new MeanShiftFloatPointsShader(
+                    pointsBuffer as ReadWriteBuffer<float>,
+                    fieldBuffer as ReadOnlyBuffer<float>,
+                    fieldWeights as ReadWriteTexture2D<float>,
+                    flagBuffer, kernel)),
+            _ => null,
+        };
+
+        // The step method could not be created because an invalid type T was supplied
+        if (step is null)
+            ThrowHelper.ThrowArgumentOutOfRangeException(nameof(T));
 
         do
         {
             // Reset the flag
             flagBuffer.CopyFrom(source);
 
-            device.For(pointsBuffer.Length, shader);
+            // Execute step
+            step();
 
             // Retrieve the flag
             flagBuffer.CopyTo(readBack);
